47cc8c24c46684429e743c8f6293f09bd85f034c
Wu Yuli <xuanyuwuyao@gmail.com>
2022-04-10 21:46:01 +1200

    修好了继承的bug 基本结构的抽象类继承和模块化完成

--- world/mesh_world/mesh_world.py
+++ world/mesh_world/mesh_world.py
@@ -3,7 +3,7 @@ from world.world import World
 
 class Mesh_world(World):
     def __init__(self, state):
-        super(state, self).__init__(self, state)
+        super(Mesh_world, self).__init__(state)
 
     def evolution_a_turn(self):
         print("mesh evolution_a_turn")
