52e60f4b78c3eca46e77729b80dda3b899ab2c44
K-Makitsuki <abc12321liaosha@126.com>
2022-04-10 19:38:53 +0800

    修改了子类世界的名字

--- entry.py
+++ entry.py
@@ -2,7 +2,7 @@ import importlib
 
 
 def get_generator(generator_file):
-    generator_file = 'world.' + generator_file + '.' + generator_file + '_generator'
+    generator_file = 'world.' + generator_file + '.' + 'world_generator'
     generator_module = importlib.import_module(generator_file)
     # from world.mesh_world.mesh_world_generator import Concrete_world_generator as Generator
     return generator_module.Concrete_world_generator()
