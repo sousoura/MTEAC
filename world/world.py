5f91cfa3b63e8226f5eb7faffef430ff163a1be8
Wu Yuli <xuanyuwuyao@gmail.com>
2022-04-10 18:30:36 +1200

    更新了基本文件架构，把抽象规定文件都放到了world文件夹里，同时创建了mesh_world文件夹存放具体类 目前的架构为世界、状态和生成器三个类

--- world/world.py
+++ world/world.py
@@ -3,9 +3,13 @@ from abc import ABCMeta, abstractmethod
 
 class world(metaclass=ABCMeta):
     @abstractmethod
-    def __init__(self):
+    def __init__(self, state):
         pass
 
     @abstractmethod
-    def __init__(self):
+    def evolution(self):
+        pass
+
+    @abstractmethod
+    def expansion(self):
         pass
