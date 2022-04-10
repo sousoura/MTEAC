73a9f7a56489d39a7aec05e69541dc77fda3c569
Wu Yuli <xuanyuwuyao@gmail.com>
2022-04-10 17:36:07 +1200

    初始化世界类、世界生成器的抽象类和main的文件

---
+++ world.py
@@ -0,0 +1,11 @@
+from abc import ABCMeta, abstractmethod
+
+
+class world(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __init__(self):
+        pass
